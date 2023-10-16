5484f723f376895b65c31c379e372269fdbb37d0
Ethan Lew <15167713+ethanl21@users.noreply.github.com>
2023-10-15 20:57:36 -0700

    Add example GDExtension project

--- src/RegisterExtension.cpp
+++ src/RegisterExtension.cpp
@@ -6,46 +6,39 @@
 #include "godot_cpp/core/defs.hpp"
 #include "godot_cpp/godot.hpp"
 
-#include "Example.h"
+#include "GDExample.h"
 #include "GDExtensionTemplate.h"
+
+using namespace godot;
 
 /// @file
 /// Register our classes with Godot.
 
-namespace
+/// @brief Called by Godot to let us register our classes with Godot.
+///
+/// @param p_level the level being initialized by Godot
+///
+/// @see GDExtensionInit
+void initialize_gdextension_types(ModuleInitializationLevel p_level)
 {
-    /// @brief Called by Godot to let us register our classes with Godot.
-    ///
-    /// @param p_level the level being initialized by Godot
-    ///
-    /// @see GDExtensionInit
-    void initializeExtension( godot::ModuleInitializationLevel p_level )
-    {
-        if ( p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE )
-        {
-            return;
-        }
-
-        godot::ClassDB::register_class<ExampleRef>();
-        godot::ClassDB::register_class<ExampleMin>();
-        godot::ClassDB::register_class<Example>();
-        godot::ClassDB::register_class<ExampleVirtual>( true );
-        godot::ClassDB::register_abstract_class<ExampleAbstract>();
-
-        godot::ClassDB::register_class<GDExtensionTemplate>();
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
     }
+    //ClassDB::register_class<YourClass>();
+    ClassDB::register_class<GDExample>();
 
-    /// @brief Called by Godot to let us do any cleanup.
-    ///
-    /// @see GDExtensionInit
-    void uninitializeExtension( godot::ModuleInitializationLevel p_level )
-    {
-        if ( p_level != godot::MODULE_INITIALIZATION_LEVEL_SCENE )
-        {
-            return;
-        }
+    godot::ClassDB::register_class<GDExtensionTemplate>();
+}
+
+/// @brief Called by Godot to let us do any cleanup.
+///
+/// @see GDExtensionInit
+void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
     }
 }
+
 
 extern "C"
 {
@@ -59,20 +52,30 @@ extern "C"
     /// @param r_initialization the intialization (need more info)
     ///
     /// @returns GDExtensionBool
-    GDExtensionBool GDE_EXPORT GDExtensionInit(
-        GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library,
-        GDExtensionInitialization *r_initialization )
+//    GDExtensionBool GDE_EXPORT GDExtensionInit(
+//        GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library,
+//        GDExtensionInitialization *r_initialization )
+//    {
+//        {
+//            godot::GDExtensionBinding::InitObject init_obj( p_get_proc_address, p_library,
+//                                                            r_initialization );
+//
+//            init_obj.register_initializer( initializeExtension );
+//            init_obj.register_terminator( uninitializeExtension );
+//            init_obj.set_minimum_library_initialization_level(
+//                godot::MODULE_INITIALIZATION_LEVEL_SCENE );
+//
+//            return init_obj.init();
+//        }
+//    }
+
+    GDExtensionBool GDE_EXPORT GDExtensionInit(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
     {
-        {
-            godot::GDExtensionBinding::InitObject init_obj( p_get_proc_address, p_library,
-                                                            r_initialization );
+        GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+        init_obj.register_initializer(initialize_gdextension_types);
+        init_obj.register_terminator(uninitialize_gdextension_types);
+        init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
 
-            init_obj.register_initializer( initializeExtension );
-            init_obj.register_terminator( uninitializeExtension );
-            init_obj.set_minimum_library_initialization_level(
-                godot::MODULE_INITIALIZATION_LEVEL_SCENE );
-
-            return init_obj.init();
-        }
+        return init_obj.init();
     }
 }
